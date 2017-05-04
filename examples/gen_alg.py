d1e55618f68b3a646dfd14c985dfe330335302ed
augoff <michal.augoff@gmail.com>
2017-05-04 15:27:13 +0200

    fitness function moved from example to utils

--- examples/gen_alg.py
+++ examples/gen_alg.py
@@ -1,18 +1,28 @@
-from kheppy.evocom.commons.nn import NeuralNet
-from kheppy.evocom.ga import GeneticAlgorithm
 import numpy as np
 
-
-def avoid_collision(sensors, left_motor, right_motor):
-    speed_factor = (abs(left_motor) + abs(right_motor)) / 2
-    movement_factor = 1 - np.sqrt(abs(left_motor - right_motor) / 2)
-    proximity_factor = 1 - np.sqrt(max(sensors))
-    return speed_factor * movement_factor * proximity_factor
+from kheppy.evocom.commons.nn import NeuralNet
+from kheppy.evocom.ga import GeneticAlgorithm
+from kheppy.utils.fitfunc import avoid_collision
 
 
 if __name__ == '__main__':
     model = NeuralNet(8).add_layer(30, 'relu').add_layer(2, 'tanh')
-    ga = GeneticAlgorithm()
-    ga.eval_params(model, avoid_collision).sim_params('worlds/circles.wd', 1, 5).evo_params(epochs=5)
-    ga.run('/home/user/kheppy_results/', verbose=True)
-    ga.test(num_points=100, verbose=True)
+    # params = [4]
+    # for p in params:
+    res = []
+    rep = GeneticAlgorithm().reporter
+    for i in range(10, 30):
+        ga = GeneticAlgorithm()
+        ga.reporter = rep
+        ga.sim_params('worlds/circle.wd', 1, 5)
+        ga.eval_params(model, avoid_collision, num_positions=1)
+        ga.evo_params(pop_size=150, p_mut=0.02, p_cross=0.75, epochs=1, param_init_limits=(-3, 3))
+        ga.sel_params(sel_type=6)
+        ga.run(num_proc=2, seed=i, verbose=True)
+        test_res = ga.test(num_points=1)
+        res.append(test_res)
+        if i != 29:
+            rep.move_to_new_series()
+    rep.save('/Users/augoff/Dropbox/QA/private-research/master_thesis/results/1/static/all_seeds.rep')
+    print('[' + ', '.join(['{:.4f}'.format(x) for x in res]) + ']')
+    print('avg test fitness = {:.4f}'.format(np.mean(res)))

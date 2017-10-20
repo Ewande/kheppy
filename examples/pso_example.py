91bffdd54fd1d87f8ac76d102de4f71af90df97e
augoff <michal.augoff@gmail.com>
2017-10-20 15:50:11 +0200

    minor example update

--- examples/pso_example.py
+++ examples/pso_example.py
@@ -6,7 +6,7 @@ from kheppy.utils.fitfunc import avoid_collision
 if __name__ == '__main__':
     model = NeuralNet(8).add_layer(30, 'relu').add_layer(2, 'tanh')
     pso = PartSwarmOpt()
-    pso.eval_params(model, avoid_collision).sim_params('worlds/circle.wd', 1, 5).main_params(epochs=5)
+    pso.eval_params(model, avoid_collision).sim_params('worlds/circle.wd', 1, 5).main_params(max_epochs=5)
     pso.pso_params()  # set PSO-specific parameters here
     pso.run('/home/user/kheppy_results/', verbose=True)
     pso.test(num_points=100, verbose=True)
